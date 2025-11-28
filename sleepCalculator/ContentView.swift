3d2fbdf318f9a90ca3b9cf1e5bdb79872b8ccf6d
Austine Huang <austine.huang@gmail.com>
2025-11-28 10:41:12 +0800

    Initial Commit

---
+++ sleepCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleepCalculator
+//
+//  Created by Austine Huang on 2025/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleepCalculator/sleepCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepCalculatorApp.swift
+//  sleepCalculator
+//
+//  Created by Austine Huang on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct sleepCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
